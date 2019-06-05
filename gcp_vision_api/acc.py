8bfc35a701feb573afeea83d1da5efc46f34283e
aadesh <aadeshkale0@gmail.com>
2019-06-05 17:08:43 +0530

    google vision api

---
+++ gcp_vision_api/acc.py
@@ -0,0 +1,26 @@
+import io
+from google.oauth2 import service_account
+from google.cloud import vision
+from google.cloud.vision import types
+
+# import os
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "notes-all.json"
+
+cread = service_account.Credentials.from_service_account_file('notes-all.json')
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient(credentials=cread)
+
+# Loads the image into memory
+with io.open('test.jpg', 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
---
+++ gcp_vision_api/readme.md
@@ -0,0 +1,9 @@
+
+This script is used to simply demostratate the google vision api
+-----------------
+
+1.Please fill "notes-all.json" with valid google cloud credentials with vision api access
+
+2.Install all requirements 
+
+3.follow all comments in "acc.py" program itself 
